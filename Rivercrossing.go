73fe3eca79106278438ba70fcb05b8af63974e38
Mirrea <miriam_lande@hotmail.no>
2022-02-13 17:44:17 +0100

    dette er en committ

---
+++ Rivercrossing.go
@@ -0,0 +1,9 @@
+package del3
+"Dette er en add"
+
+package state
+
+func ViewState() string {
+	// Sjekke data som er lagret ("kylling til venstre", "rev til venstre")
+	return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+}
